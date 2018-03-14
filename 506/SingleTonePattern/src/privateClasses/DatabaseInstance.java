3d2662485a3ee980453a455ddcb10f93f3b7cef6
atiqahammed <atiqahammedshamim@gmail.com>
2018-03-14 06:12:03 -0700

    double checking added

--- 506/SingleTonePattern/src/privateClasses/DatabaseInstance.java
+++ 506/SingleTonePattern/src/privateClasses/DatabaseInstance.java
@@ -2,10 +2,18 @@ package privateClasses;
 
 public class DatabaseInstance {
 
-	private static DatabaseInstance databaseInstance = new DatabaseInstance();
+	private static DatabaseInstance databaseInstance;
 	private DatabaseInstance(){};
 	
 	public static DatabaseInstance getDatabaseInstance() {
+		if(databaseInstance == null) {
+			synchronized (DatabaseInstance.class) {
+				if(databaseInstance == null) {
+					databaseInstance = new DatabaseInstance();
+				}
+				return databaseInstance;
+			}
+		}
 		return databaseInstance;
 	}
 	
