029a803ab2edfec13a81a4606ac268194c4a0fba
atiqahammed <atiqahammedshamim@gmail.com>
2018-03-14 07:13:43 -0700

    specifically points modified

--- 506/CompositPattern/src/allShapes/Rectangle.java
+++ 506/CompositPattern/src/allShapes/Rectangle.java
@@ -7,10 +7,10 @@ public class Rectangle extends CompositShape{
 
 	@Override
 	public void buildShap() {
-		shapes.add(new Line());
-		shapes.add(new Line());
-		shapes.add(new Line());
-		shapes.add(new Line());
+		shapes.add(new Line(new Point(0, 0), new Point(50, 0)));
+		shapes.add(new Line(new Point(50, 0), new Point(50, 50)));
+		shapes.add(new Line(new Point(0, 0), new Point(0, 50)));
+		shapes.add(new Line(new Point(0, 50), new Point(50, 50)));
 	}
 
 }
