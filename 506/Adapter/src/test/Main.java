281c2938620c5a0d20e05160b826c3e3d6f8c735
atiqahammed <atiqahammedshamim@gmail.com>
2018-03-20 23:17:17 -0700

    bad solution of adapter

---
+++ 506/Adapter/src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Main {
+	 public static void main(String[] args) {
+		
+		 MP3Player mp3Player = new MP3Player();
+		 MP4Player mp4Player = new MP4Player();
+		 VLCPlayer vlcPlayer = new VLCPlayer();
+		 
+		 MyPlayer  myPlayer = new MyPlayer(vlcPlayer);
+		 
+		 myPlayer.play();
+		 
+	}
+}
