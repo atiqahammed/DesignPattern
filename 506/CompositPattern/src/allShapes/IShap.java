088b620ecf10671b4bb4890ffeed9e8ea8accb5f
atiqahammed <atiqahammedshamim@gmail.com>
2018-03-27 00:06:10 -0700

    factory pattern has been added

--- 506/CompositPattern/src/allShapes/IShap.java
+++ 506/CompositPattern/src/allShapes/IShap.java
@@ -1,5 +1,9 @@
 package allShapes;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 public interface IShap {
 	public void drow();
+	public void drow(Graphics2D graphics);
 }
--- 506/CompositPattern/src/allShapes/Line.java
+++ 506/CompositPattern/src/allShapes/Line.java
@@ -1,12 +1,25 @@
 package allShapes;
 
-public class Line implements IShap{
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+public class Line implements IShap {
 	private Point start;
 	private Point end;
-	
+
 	@Override
 	public void drow() {
-		System.out.println("drow a line start from ("+start.getxCoOrdinate()+", "+ start.getyCoOrdinate()+") to (" + end.getxCoOrdinate()+", "+ end.getyCoOrdinate()+")");
+		System.out.println("drow a line start from (" + start.getxCoOrdinate() + ", " + start.getyCoOrdinate()
+				+ ") to (" + end.getxCoOrdinate() + ", " + end.getyCoOrdinate() + ")");
+	}
+
+	@Override
+	public void drow(Graphics2D graphics) {
+		graphics.draw(new Line2D.Float(start.getxCoOrdinate(), start.getyCoOrdinate(), end.getxCoOrdinate(),
+				end.getyCoOrdinate()));
 	}
 
 	public Line(Point start, Point end) {
